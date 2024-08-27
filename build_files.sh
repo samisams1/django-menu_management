9f61a5ed65ece6bfc321f4095c48508e6f5ec1ed
tedamami <samsonmamushet3@gmail.com>
2024-08-27 14:24:06 +0300

    deployee to vercel

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,11 @@
-pip install -r requirements.txt 
-python3 manage.py collectstatic
+#!/bin/bash
+
+# Activate the virtual environment
+python3 -m venv .venv
+source .venv/bin/activate
+
+# Install the required packages
+pip install -r requirements.txt
+
+# Collect static files
+python3 manage.py collectstatic --no-input
