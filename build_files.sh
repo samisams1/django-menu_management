b64132c4e7dbabba717aedee1ef338f8edcb7e5a
tedamami <samsonmamushet3@gmail.com>
2024-08-27 11:57:49 +0300

    deployee to vercel

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,5 @@
 # Activate the virtual environment (if applicable)
 # source path/to/venv/bin/activate
 
-# Install requirements
-python3 -m pip install -r requirements.txt
-
-# Collect static files
-python3 manage.py collectstatic --no-input
+# Install requirements without the 'apturl' package
+python3 -m pip install -r requirements.txt --no-deps --upgrade apturl
